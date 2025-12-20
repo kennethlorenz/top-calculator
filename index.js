6deb5daac9be3bafa83b84efa3b725a8166b9bb6
kz <kennethlorenzg@gmail.com>
2025-12-20 13:44:01 -0500

    add operators and operate function

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+      subtract(firstNumber, secondNumber);
+      break;
+    case "x":
+      multiply(firstNumber, secondNumber);
+      break;
+    case "÷":
+      divide(firstNumber, secondNumber);
+      break;
+  }
+}
